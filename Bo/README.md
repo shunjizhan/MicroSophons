f40e7c90161775626f8056b9eb10a6846a9197d7
boluan <bo_luan@umail.ucsb.edu>
2016-10-10 23:00:08 -0700

    playing with javascript

--- Bo/README.md
+++ Bo/README.md
@@ -1,1 +1,3 @@
 # Microsoft-Capstone
+
+This is test directory for Bo Luan's javascript practices!
---
+++ Bo/main.js
@@ -0,0 +1,8 @@
+function main(){
+    console.log("yes");
+    alert("Bazinga!");
+    
+}
+
+console.log("in js");
+$(document).ready(main);
