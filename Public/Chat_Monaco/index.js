d86ae209a224768d01615b2be509ba04250c59a4
shunjizhan <shunjizhan@umail.ucsb.edu>
2016-11-09 21:00:07 -0800

    change method

--- Public/Chat_Monaco/index.js
+++ Public/Chat_Monaco/index.js
@@ -8,74 +8,136 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
 
-//app.use(express.static('js'));
-app.use(express.static(path.join(__dirname, '.')));
+var fs = require("fs");
+var ini = false;
+var u = [];
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
-});
+app.use(express.static(path.join(__dirname, '.'))); //app.use(express.static('js'));
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-
-  socket.on('chat message', function(msg){
-    // console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
-
-  socket.on('cursor',function(msg){
-	   io.emit('cursor',msg);
-  });
-
-  socket.on('user',function(data){
-     io.emit('user', data);
-  });
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-
-
-
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
 
-/*
-app.get('/', function(req, res){
-  res.sendfile('index.html');
+io.on('connection', function(socket) {
+    console.log('a user connected');
+    if (ini == false) { 
+        ini_json();     // initialize JSON
+        ini = true;
+        console.log('initialized JSON!')
+    } 
+
+    var this_user_name = "ShaB" + Math.floor(Math.random() * 20);
+    write(this_user_name);  // write this user to JSON
+    update_users();  // emit events for clients to update users
+  
+
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('cursor',function(msg) {
+        io.emit('cursor',msg);
+    });
+
+    socket.on('user',function(new_name) {
+       // alert("on user!");
+        delete_user(this_user_name);
+        this_user_name = new_name;
+        write(this_user_name);
+        update_users();
+    });
+
+    socket.on('disconnect', function() {
+        console.log('disconnected:' + this_user_name);
+        delete_user(this_user_name);
+    });
+
 });
-*/
 
+function ini_json() {
+    var empty = {'users': []};
+    fs.writeFile( "users.json", JSON.stringify(empty), "utf8", null );
+}
 
-/* load the index.html */
-/*
-var fs = require("fs");
-var http = require("http");
-var url = require("url");
+function write(name) {
+    fs.readFile("users.json", 'utf8', function readFileCallback(err, data){
+        if (err){ 
+            console.log(err); 
+        } 
+        else {
+            obj = JSON.parse(data); //now it an object
+            user = { "name": name };
+            obj['users'].push(user);
+            var users = JSON.stringify(obj); //convert it back to json
+            fs.writeFile( "users.json", users, "utf8", null );
 
-http.createServer(function (request, response) {
+            console.log(users);
+        }
+    });
 
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
+}
 
-    response.writeHead(200);
+function delete_user(name) {
+    fs.readFile("users.json", 'utf8', function readFileCallback(err, data){
+        if (err) { 
+            console.log(err); 
+        } 
+        else {
+            obj = JSON.parse(data); //now it an object
+            console.log(data);
+            findAndRemove(obj.users, 'name', name);
+            var users = JSON.stringify(obj); //convert it back to json
+            fs.writeFile( "users.json", users, "utf8", null );
+        }
+    });
+}
 
-    if(pathname == "/") {
-        html = fs.readFileSync("index.html", "utf8");
-        response.write(html);
-    } else if (pathname == "/script.js") {
-        script = fs.readFileSync("script.js", "utf8");
-        response.write(script);
-    }
+function findAndRemove(array, property, value) {
+    array.forEach(function(result, index) {
+        if(result[property] === value) {
+            array.splice(index, 1);
+        }    
+    });
+}
+
+function update_users() {
+    var users = [];
+    fs.readFile("users.json", 'utf8', function readFileCallback(err, data){
+        if (err) { 
+            console.log(err); 
+        } 
+        else {
+            obj = JSON.parse(data); //now it an object
+            //console.log(data);
+            obj.users.forEach( function(element, index) {
+                console.log(element);
+                //users.push(element.name);
+            });   
+
+            console.log('updated users, current users:')
+            console.log(users);
+        }
+    });
+
+    //io.emit('user', users);
+
     
-    response.end();
-}).listen(3000);
+}
 
-console.log("Listening to server on 8888...");
-*/
+
+
+
+
+
+
+
+
+
+
 
 
 
--- Public/Chat_Monaco/js/load_socket.js
+++ Public/Chat_Monaco/js/load_socket.js
@@ -1,4 +1,3 @@
-
 var socket = io();
 
 $(document).ready(socket_function);
@@ -6,53 +5,53 @@ $(document).ready(socket_function);
 function socket_function(){
     // send message
     $('form#send').submit(function(){
-      var id = $('#name').text();
-      msg =  id + ": " + $('#m').val();
-      console.log(msg)
-      socket.emit('chat message', msg); 
-      $('#m').val('');
-      return false;
+       var id = $('#name').text();
+       msg =  id + ": " + $('#m').val();
+       socket.emit('chat message', msg); 
+       $('#m').val('');
+       return false;
     });
 
     socket.on('chat message', function(msg){  
-      $('#messages').append($('<li>').text(msg));
+       $('#messages').append($('<li>').text(msg));
     });
 
-    // when there is user name change, emit user_change event to everyone
+
+    // when there is user name change, emit user event to server
     $('form#user_form').submit(function(){  
-      var del = '#' + $('#prev').text()
-      var name = $('#user_name').val();
-      $('#user_name').val('');
+       // var del = '#' + $('#prev').text()
+       var name = $('#user_name').val();
+       $('#user_name').val('');
 
-      var del = '#' + $('#prev').text()
+       // var del = '#' + $('#prev').text()
 
-      $('#name').html(name);    // set user name
-      $('#prev').text(name); // save the user name locally   
-      socket.emit('user', {name: name, del: del}); 
+       $('#name').html(name);    // set user name
+       // $('#prev').text(name); // save the user name locally   
+       socket.emit('user', name); 
 
-      return false;
+       return false;
     });
 
-    socket.on('user', function(data){  
-      $('#online_users').append($('<li>').attr('id', data.name).text(data.name)); // update the online users
-      $(data.del).remove(); // remove the previous name 
+    socket.on('user', function(users){  // users is an array containing all user names
+       users.forEach( function(user, index) {
+          $('#online_users').append($('<li>').attr('id', user).text("user")); // update the online users
+       });
+       $(data.del).remove(); // remove the previous name 
     });
 
-    /*
-    socket.on('user_leave', function(del){
-      $(del).remove();
-    });
-    */
+
     // default name
+    /*
     name = "ShaB" + Math.floor(Math.random() * 20);
     $('#name').html(name); 
     $('#prev').text(name);
-	  socket.emit('user', {name: name, del: ""});
+	socket.emit('user', {name: name, del: ""});
+    */
+
+   
     
 
 }
-
-
 
 
 
