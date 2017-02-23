331857222101a0233425c97de0d3f52ceac1b201
boluan <bo_luan@umail.ucsb.edu>
2017-02-23 15:19:37 -0800

    deal with 'pseudo-disconnection'

--- Zixia/Chat_Monaco/js/ping.js
+++ Zixia/Chat_Monaco/js/ping.js
@@ -1,6 +1,6 @@
 var received = false;
 
-var interval = setInterval(ping, 5000);
+var interval = setInterval(ping, 10000)
 
 var time_send;
 var latency;
@@ -9,26 +9,26 @@ function ping(){
 	socket.emit("my-ping","");
 	received = false;
 	time_send = Date.now();
-	var timeout = setTimeout(checkReceive, 4900);
+	var timeout = setTimeout(checkReceive, 9900);
 }
 
 function checkReceive(){
 	if(!received){
 		console.log('ping time out');
-		$('#disconnect').show();
-	    for (var i = editors.length - 1; i >= 0; i--) {
-	        editors[i].updateOptions({readOnly:true});
-	    }
-	    socket.connect();
+		// $('#disconnect').show();
+	 //    for (var i = editors.length - 1; i >= 0; i--) {
+	 //        editors[i].updateOptions({readOnly:true});
+	 //    }
+	    // socket.connect();
 
 	}
-	else{
-		$('#disconnect').hide();
-	    for (var i = editors.length - 1; i >= 0; i--) {
-	        editors[i].updateOptions({readOnly:false});
-	    }
+	// else{
+	// 	$('#disconnect').hide();
+	//     for (var i = editors.length - 1; i >= 0; i--) {
+	//         editors[i].updateOptions({readOnly:false});
+	//     }
 	   	
-	}
+	// }
 }
 
 socket.on('my-ping', function(msg){
